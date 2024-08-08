559a66ffa6b7cd4fc1549cbab6540f058b56f35c
umermalik053 <um4273209@gmail.com>
2024-08-08 19:07:56 +0500

    login sigup complete

---
+++ script.js
@@ -0,0 +1,54 @@
+import { app } from "./scripts/firebase.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+const auth = getAuth(app);
+
+let name = document.querySelector("#name");
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let create_acc = document.querySelector("#create-acc");
+
+if (create_acc) {
+  create_acc.addEventListener("click", () => {
+    createUserWithEmailAndPassword(auth, email.value, password.value)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        console.log("account register succesfully");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        console.log(errorCode);
+        console.log("account Register Failed");
+      });
+  });
+}
+
+let lemail = document.querySelector("#lemail");
+let lpassword = document.querySelector("#lpassword");
+let loginBtn = document.querySelector("#login-acc");
+
+if (loginBtn) {
+  loginBtn.addEventListener("click", () => {
+    signInWithEmailAndPassword(auth, lemail.value, lpassword.value)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log("login succesfully");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log("sorry login failed");
+      });
+  });
+}
--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,6 +1,10 @@
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-  import { getAuth,createUserWithEmailAndPassword , signInWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
   const firebaseConfig = {
     apiKey: "AIzaSyBXGVRGF9A84_tMSJw0eHNoaqDJ2qIeMnI",
     authDomain: "ecommerce-ee910.firebaseapp.com",
@@ -12,59 +16,77 @@
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
+export {app}
+
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+  // import { getAuth,createUserWithEmailAndPassword , signInWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyBXGVRGF9A84_tMSJw0eHNoaqDJ2qIeMnI",
+  //   authDomain: "ecommerce-ee910.firebaseapp.com",
+  //   projectId: "ecommerce-ee910",
+  //   storageBucket: "ecommerce-ee910.appspot.com",
+  //   messagingSenderId: "538725033874",
+  //   appId: "1:538725033874:web:24fa26440170dfc2ce434b"
+  // };
+
+  // // Initialize Firebase
+  // const app = initializeApp(firebaseConfig);
+  // const auth = getAuth(app);
 
 
-  let name = document.querySelector("#name")
-  let email = document.querySelector("#email")
-  let password = document.querySelector("#password")
-  let create_acc = document.querySelector("#create-acc")
-  let login = document.querySelector("#login-acc")
+  // let name = document.querySelector("#name")
+  // let email = document.querySelector("#email")
+  // let password = document.querySelector("#password")
+  // let create_acc = document.querySelector("#create-acc")
+  // let login = document.querySelector("#login-acc")
+
+  // console.log(lemail, lpassword)
 
 
  
 
-  create_acc.addEventListener("click",()=>{
-    console.log(name.value)
-    console.log(email.value)
-    console.log(password.value)
+  // // create_acc.addEventListener("click",()=>{
+  // //   console.log(name.value)
+  // //   console.log(email.value)
+  // //   console.log(password.value)
 
-    createUserWithEmailAndPassword(auth, email.value, password.value)
-  .then((userCredential) => {
-    // Signed up 
-    const user = userCredential.user;
-    console.log("user--->",user)
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log("err--->",errorMessage)
-    // ..
-  });
+  // //   createUserWithEmailAndPassword(auth, email.value, password.value)
+  // // .then((userCredential) => {
+  // //   // Signed up 
+  // //   const user = userCredential.user;
+  // //   console.log("user--->",user)
+  // //   // ...
+  // // })
+  // // .catch((error) => {
+  // //   const errorCode = error.code;
+  // //   const errorMessage = error.message;
+  // //   console.log("err--->",errorMessage)
+  // //   // ..
+  // // });
 
 
-  })
+  // // })
 
 
   
-  login.addEventListener("click" , ()=>{
-    let e = email.value;
-    let pass = password.value
-    signInWithEmailAndPassword(auth, e, pass)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    console.log(user);
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorMessage);
+  // // login.addEventListener("click" , ()=>{
+  // //   let e = email.value;
+  // //   let pass = password.value
+  // //   signInWithEmailAndPassword(auth, e, pass)
+  // // .then((userCredential) => {
+  // //   // Signed in 
+  // //   const user = userCredential.user;
+  // //   console.log(user);
+  // //   // ...
+  // // })
+  // // .catch((error) => {
+  // //   const errorCode = error.code;
+  // //   const errorMessage = error.message;
+  // //   console.log(errorMessage);
     
-  });
-  })
+  // // });
+  // // })
 
 
 
